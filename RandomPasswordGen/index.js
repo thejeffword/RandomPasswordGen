5addc71834c3010e5f3378d92e8e6250f11d6e7a
thejeffword <42900889+thejeffword@users.noreply.github.com>
2022-05-10 09:07:53 -0700

    Update index.js

--- RandomPasswordGen/index.js
+++ RandomPasswordGen/index.js
@@ -1,10 +1,3 @@
-//defining html variables in js
-// let passWord1 = document.getElementById("pw-1")
-// let passWord2 = document.getElementById("pw-2")
-// let passWord3 = document.getElementById("pw-3")
-// let passWord4 = document.getElementById("pw-4")
-//unused right now because couldn't get textContent to properly replace.  ended up just directly referencing the html element ids instead of assigning them.
-
 //catalog of characters for generation
 let bank = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","!","@","#","$","%","^","&","*","?","+","1","2","3","4","5","6","7","8","9","0"];
 
@@ -38,13 +31,5 @@ function displayPasswords() {
     document.getElementById("pw-2").innerHTML = generatedPW2
     document.getElementById("pw-3").innerHTML = generatedPW3
     document.getElementById("pw-4").innerHTML = generatedPW4
-
-    //this is really hacky. there's a better way to run the function 4 different times without writing it 4 times.
-
-    // passWord1.textContent = "Poop"
-    // passWord2.textContent = "Fart"
-    // passWord3.textContent = "Shit"
-    // passWord4.textContent = "Ass"
-    //unused right now because couldn't get textContent to properly replace.  ended up just directly referencing the html element ids instead of assigning them.
 }
 
